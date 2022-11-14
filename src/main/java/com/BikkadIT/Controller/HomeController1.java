622ab200d4697bb2645cabcfcd40c37b5293413d
dhangare12 <jyotidhangare00@gmail.com>
2022-11-14 14:41:19 +0530

    testing complete

--- src/main/java/com/BikkadIT/Controller/HomeController1.java
+++ src/main/java/com/BikkadIT/Controller/HomeController1.java
@@ -1,9 +1,9 @@
-package com.BikkadIT.controller;
+package com.BikkadIT.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
-public class HomeController {
+public class HomeController1 {
 	@GetMapping("/home")
 	public String home() {
 		String hello="Good Morning ";
--- src/main/java/com/BikkadIT/Controller/WelcomeController1.java
+++ src/main/java/com/BikkadIT/Controller/WelcomeController1.java
@@ -1,10 +1,10 @@
-package com.BikkadIT.controller;
+package com.BikkadIT.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class WelcomeController {
+public class WelcomeController1 {
     @GetMapping("/msg")
 	public String msg() {
 		String hi="Welcome to BikkadIT ";
