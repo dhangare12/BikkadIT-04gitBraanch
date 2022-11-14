8933f64663e170e1231e4e76dc5e0104716594fd
dhangare12 <jyotidhangare00@gmail.com>
2022-11-14 13:52:37 +0530

    Home Controller Added

--- src/main/java/com/BikkadIT/controller/HomeController.java
+++ src/main/java/com/BikkadIT/controller/HomeController.java
@@ -2,13 +2,12 @@ package com.BikkadIT.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
 @Controller
-public class WelcomeController {
-    @GetMapping("/msg")
-	public String msg() {
-		String hi="Welcome to BikkadIT ";
-    	return hi;
+public class HomeController {
+	@GetMapping("/home")
+	public String home() {
+		String hello="Good Morning ";
+    	return hello;
 		
-	}
+}
 }
